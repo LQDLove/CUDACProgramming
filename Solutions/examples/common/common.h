0a43be5febaec90410237b91b5021cc4dfa1b3dd
fengxiaoxiaokun <fengxiaoxiaokun@hotmail.com>
2016-05-22 07:51:20 +0800

    run windows

--- Solutions/examples/common/common.h
+++ Solutions/examples/common/common.h
@@ -66,8 +66,8 @@
 
 inline double seconds()
 {
-    struct timeval tp;
-    struct timezone tzp;
+    struct timeval2 tp;
+    struct timezone2 tzp;
     int i = gettimeofday(&tp, &tzp);
     return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
 }
--- Solutions/examples/common/wtime.h
+++ Solutions/examples/common/wtime.h
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+// #include "stdafx.h"
 #include <time.h>
 #include <windows.h> 
 
